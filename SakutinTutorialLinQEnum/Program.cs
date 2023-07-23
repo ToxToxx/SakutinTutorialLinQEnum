475324205c67671cadf9d608015136a57461bbc7
ToxToxx <107263231+ToxToxx@users.noreply.github.com>
2023-07-23 15:08:11 +0300

    Добавьте файлы проекта.

---
+++ SakutinTutorialLinQEnum/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SakutinTutorialLinQEnum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Enum
+            List<Game> games = new List<Game>
+            {
+                new Game("Black & White", Genre.Strategy),
+                new Game("The Witcher 3", Genre.RPG),
+                new Game("Civilization 5", Genre.Strategy),
+                new Game("Obscure", Genre.Horror)
+            };
+
+            foreach (Game game in games)
+            {
+                game.ShowInfo();
+            }
+
+            Console.WriteLine((Genre)3);
+
+            //LinQ
+        }
+    }
+
+    enum Genre//Тип значение определенный набором именованных констант
+    {
+        Strategy = 1,
+        RPG,
+        Horror
+    }
+
+    class Game
+    {
+        private string _title;
+        private Genre _genre;
+
+        public Game(string title, Genre genre)
+        {
+            _title = title;
+            _genre = genre;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Это игра: {_title}, её жанр {_genre}");
+        }
+    }
+}
